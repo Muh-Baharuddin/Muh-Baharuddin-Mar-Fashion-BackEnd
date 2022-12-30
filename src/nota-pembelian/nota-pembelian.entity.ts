50590b3cea65a8646fde5fc0cf5d1bdce564737f
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2022-12-30 18:32:15 +0700

    membuat relasi OneToMany table nota pembelian dan ManyToOne table daftar supplier

--- src/nota-pembelian/nota-pembelian.entity.ts
+++ src/nota-pembelian/nota-pembelian.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  JoinTable,
+  OneToMany,
+} from 'typeorm';
+import { DaftarSupplier } from 'src/daftar-supplier/daftar-supplier.entity';
 
 @Entity()
 export class NotaPembelian {
@@ -7,4 +14,11 @@ export class NotaPembelian {
 
   @Column()
   tanggal_pembelian: string;
+
+  @Column()
+  total_pengeluaran: string;
+
+  @OneToMany(() => DaftarSupplier, (supplier) => supplier.notaPembelian)
+  @JoinTable()
+  suppliers: DaftarSupplier[];
 }
